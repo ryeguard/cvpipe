a40cf52e56af32fd5dd7c434590133508043b142
Simon Rygård <simon.rygard+dev.github@gmail.com>
2024-03-20 22:45:58 +0100

    feat: iterate on pipe/filter/mat options

--- pipe.go
+++ pipe.go
@@ -2,13 +2,20 @@ package cvpipe
 
 import (
 	"fmt"
+	"image"
 
 	"gocv.io/x/gocv"
 )
 
 type PipeOptions struct {
+	Name string
 	// Enable saving of intermediate results. Will be overridden by individual step save options.
 	Save bool
+}
+
+type FilterOptions struct {
+	// Enable saving of this step's result. Takes precedence over Pipe options
+	Save *bool
 }
 
 type PipeMat struct {
@@ -25,7 +32,7 @@ type Pipe struct {
 
 type step struct {
 	Filter
-	save bool
+	opts FilterOptions
 }
 
 func NewPipe(opts PipeOptions) *Pipe {
@@ -39,19 +46,38 @@ func (p *Pipe) Close() {
 }
 
 // Add a filter to the pipe. If save is true, the result of this step will be saved to disk.
-func (p *Pipe) Add(f Filter, save bool) *Pipe {
-	p.steps = append(p.steps, step{f, save})
+func (p *Pipe) Add(f Filter, opts ...FilterOptions) *Pipe {
+	if len(opts) == 0 {
+		p.steps = append(p.steps, step{f, FilterOptions{}})
+		return p
+	}
+
+	p.steps = append(p.steps, step{f, opts[0]})
 	return p
 }
 
+// Run applies all filters in the pipe to the given PipeMat.
 func (p *Pipe) Run(m *PipeMat) *PipeMat {
 	for i, f := range p.steps {
 		m = f.Apply(m)
-		if f.save || (f.save && p.opts.Save) {
-			gocv.IMWrite(fmt.Sprintf("data/%03d_%v.jpg", i, f.Name()), *m.mat)
+
+		var shouldSave bool
+		if f.opts.Save == nil {
+			shouldSave = p.opts.Save
+		} else {
+			shouldSave = *f.opts.Save
+		}
+
+		if shouldSave {
+			gocv.IMWrite(fmt.Sprintf("data/%v_%03d_%v.jpg", p.opts.Name, i, f.Name()), *m.mat)
 		}
 	}
 	return m
+}
+
+type MatOptions struct {
+	Size *image.Point
+	Type *gocv.MatType
 }
 
 func NewPipeMat(mat gocv.Mat) *PipeMat {
@@ -62,6 +88,22 @@ func NewPipeMat(mat gocv.Mat) *PipeMat {
 		temp:   &temp,
 		kernel: &kernel,
 	}
+}
+
+func NewPipeMatFromOptions(opts MatOptions) (*PipeMat, error) {
+	if opts.Size == nil && opts.Type == nil {
+		return nil, fmt.Errorf("Size and Type must be set")
+	}
+
+	mat := gocv.NewMatWithSize(opts.Size.Y, opts.Size.X, *opts.Type)
+	temp := gocv.NewMat()
+	kernel := gocv.NewMat()
+
+	return &PipeMat{
+		mat:    &mat,
+		temp:   &temp,
+		kernel: &kernel,
+	}, nil
 }
 
 func (m *PipeMat) Close() {
