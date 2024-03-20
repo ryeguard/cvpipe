6efee054749fcc83ef71183142be9ad20730689e
Simon Rygård <simon.rygard+dev.github@gmail.com>
2024-03-20 22:48:21 +0100

    fix: update example cmd

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"image"
-	"image/color"
 
 	"github.com/ryeguard/cvpipe"
+	"github.com/ryeguard/cvpipe/colors"
 	"gocv.io/x/gocv"
 )
 
@@ -16,11 +16,12 @@ func main() {
 	pipe := cvpipe.NewPipe(cvpipe.PipeOptions{Save: true})
 	defer pipe.Close()
 
-	pipe.
-		Add(cvpipe.NewResize(image.Pt(0, 0), 0.25, 0.25, gocv.InterpolationDefault), true).
-		Add(cvpipe.NewGaussianBlur(image.Pt(15, 15), 0, 0, gocv.BorderDefault), true).
-		Add(cvpipe.NewAdaptiveThreshold(255, gocv.AdaptiveThresholdGaussian, gocv.ThresholdBinary, 11, 2), true).
-		Add(cvpipe.NewDilate(gocv.MorphRect, image.Pt(5, 5)), true).
-		Add(cvpipe.NewPutText("Hello World!", image.Pt(10, 50), gocv.FontHersheyPlain, 1.0, color.RGBA{125, 125, 125, 125}, 2), true).
+	res := pipe.
+		Add(cvpipe.NewResize(image.Pt(0, 0), 0.25, 0.25, gocv.InterpolationDefault)).
+		Add(cvpipe.NewGaussianBlur(image.Pt(15, 15), 0, 0, gocv.BorderDefault)).
+		Add(cvpipe.NewAdaptiveThreshold(255, gocv.AdaptiveThresholdGaussian, gocv.ThresholdBinary, 11, 2)).
+		Add(cvpipe.NewDilate(gocv.MorphRect, image.Pt(5, 5))).
+		Add(cvpipe.NewPutText("Hello World!", image.Pt(10, 50), gocv.FontHersheyPlain, 1.0, colors.Grey, 2)).
 		Run(mat)
+	defer res.Close()
 }
