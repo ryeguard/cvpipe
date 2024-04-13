11c92e1a7faad4ec7169fd0d34482d253546c79d
Simon Rygård <simon.rygard+dev.github@gmail.com>
2024-04-13 16:28:52 +0200

    test: add test of Ptr()

---
+++ util/ptr_test.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPtr(t *testing.T) {
+	var tests = []struct {
+		name string
+		v    interface{}
+		want interface{}
+	}{
+		{"int", 42, Ptr(42)},
+		{"string", "hello", Ptr("hello")},
+		{"bool", true, Ptr(true)},
+	}
+	
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Ptr(tt.v)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Ptr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
